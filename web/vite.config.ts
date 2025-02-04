655bcea59bdaec9097ecf41623ca84f3820c22e3
Richard Song <brsong22@gmail.com>
2025-02-04 01:14:36 -0600

    Add allowedHosts to vite config
    - need to add our domain name deeper-sleeper.com
    to vite config allowedHosts...not sure how this was
    working this whole time until now

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -12,7 +12,11 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
     server: {
       port: 3000,
-      host: '0.0.0.0'
+      host: '0.0.0.0',
+      allowedHosts: [
+        'deeper-sleeper.com',
+        'www.deeper-sleeper.com'
+      ]
     },
     optimizeDeps: {
       exclude: ['.DS_Store'] // Ignore .DS_Store
