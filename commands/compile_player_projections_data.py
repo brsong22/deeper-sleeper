3ab77161d1dc41ad2d24def2e5891b0e7a096549
Richard Song <brsong22@gmail.com>
2025-02-05 01:58:33 -0600

    Consolidate compile projections bulk write logic
    - we don't need to make an array for player projection docs we can just
    create the array of UpdateOne operations directly and skip looping the docs
    at the end to create the operations
    - add score type so that we can differentiate between projections for
    different scoring systems

--- commands/compile_player_projections_data.py
+++ commands/compile_player_projections_data.py
@@ -2,20 +2,17 @@ import argparse
 import datetime
 import time
 from pymongo import UpdateMany, UpdateOne
-from api.utils import utils
-from api.services import sleeper
-from api.utils.utils import get_env
 from api.mongodb_client import get_db
-import os
 
 db = get_db()
 parser = argparse.ArgumentParser()
 parser.add_argument('--year', type=str, required=True, help='projections year')
+parser.add_argument('--scoring', type=str, default='ppr', help='scoring system')
 args = parser.parse_args()
 
-def compile_player_projections(year: str):
+def compile_player_projections(year: str, score_type: str):
     try:
-        projections_file_path = f'data/{year}_projections.txt'
+        projections_file_path = f'data/{year}_{score_type}_projections.txt'
         projections_data = []
         id_mappings = {}
         null_id = 0
@@ -25,9 +22,10 @@ def compile_player_projections(year: str):
             projections_data = pf.read().split(',')
         if projections_data:
             source = projections_data[0]
-            year = projections_data[1]
+            score_type = projections_data[1]
+            year = projections_data[2]
             projection_docs = []
-            players_projection_data = projections_data[2::]
+            players_projection_data = projections_data[3::]
             for player in players_projection_data:
                 player_data = player.split('_')
                 rank = player_data[0]
@@ -50,25 +48,45 @@ def compile_player_projections(year: str):
                             null_id += 1
                             print(f'null [{source}_id] for {player['player']['full_name']} ({player['id']}) - matched by full_name')
                             id_mappings[player['id']] = int(source_id)
-                        projection_docs.append({
-                            'source': source,
-                            'source_id': int(source_id),
-                            'id': player['id'],
-                            'rank': rank,
-                            'year': year
-                        }) 
+                        projection_docs.append(UpdateOne(
+                            {
+                                'id': player['id'],
+                                'year': year,
+                                'source': source,
+                                'score_type': score_type
+                            },
+                            {
+                                '$setOnInsert': {'created_at': datetime.datetime.now(datetime.timezone.utc)},
+                                '$set': {
+                                    'rank': rank,
+                                    'source_id': int(source_id),
+                                    'updated_at': datetime.datetime.now(datetime.timezone.utc)
+                                }
+                            },
+                            True
+                        ))
                     elif len(player_docs) > 1:
                         matched_by_source_id = False
                         for match in player_docs:
                             if match['player'][f'{source}_id'] == int(source_id):
                                 matched_by_source_id = True
-                                projection_docs.append({
-                                    'source': source,
-                                    'source_id': int(source_id),
-                                    'id': match['id'],
-                                    'rank': rank,
-                                    'year': year
-                                })
+                                projection_docs.append(UpdateOne(
+                                    {
+                                        'id': match['id'],
+                                        'year': year,
+                                        'source': source,
+                                        'score_type': score_type
+                                    },
+                                    {
+                                        '$setOnInsert': {'created_at': datetime.datetime.now(datetime.timezone.utc)},
+                                        '$set': {
+                                            'rank': rank,
+                                            'source_id': int(source_id),
+                                            'updated_at': datetime.datetime.now(datetime.timezone.utc)
+                                        }
+                                    },
+                                    True
+                                ))
                                 break
                         if not matched_by_source_id:
                             multi_match += 1
@@ -91,27 +109,8 @@ def compile_player_projections(year: str):
                 print(f'updating sleeper players with [{source}] ids')
                 doc_ids = db['players'].bulk_write(update_with_source_ids)
             print(f'writing player projections data')
-            projection_upserts = [
-                UpdateOne(
-                    {
-                        'id': proj['id'],
-                        'year': proj['year'],
-                        'source': proj['source']
-                    },
-                    {
-                        '$setOnInsert': {'created_at': datetime.datetime.now(datetime.timezone.utc)},
-                        '$set': {
-                            'rank': proj['rank'],
-                            'source_id': int(proj['source_id']),
-                            'updated_at': datetime.datetime.now(datetime.timezone.utc)
-                        }
-                    },
-                    True
-                )
-                for proj in projection_docs
-            ]
-            if projection_upserts:
-                doc_ids = db['player_projections'].bulk_write(projection_upserts)
+            if projection_docs:
+                doc_ids = db['player_projections'].bulk_write(projection_docs)
     except Exception as e:
         raise Exception(f'Error updating player projections data: {e}')
 
@@ -119,7 +118,7 @@ if __name__ == '__main__':
     start = time.time()
 
     print(f'>> compiling [player projections data]')
-    compile_player_projections(args.year)
+    compile_player_projections(args.year, args.scoring)
 
     end = time.time()
     print(f'=== finished in {end - start} seconds ===')
